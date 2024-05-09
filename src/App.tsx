601d76a5c8f8fdb4c5e85b3c952d66071b2d660e
GALw395 <galw395@mail.ru>
2024-05-09 15:47:18 +0300

    social_network_les_19 - route, browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import { Dialogs } from './components/Dialogs/Dialogs';
 import { Header } from './components/Header/Header';
@@ -5,18 +6,21 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Profile } from './components/Profile/Profile';
 
 const App = () => {
-  
-  return (
-    <div className="app-wrapper">
 
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+        </div>
+        {/* <Profile /> */}
+
       </div>
-      {/* <Profile /> */}
-      
-    </div>
+    </BrowserRouter>
   )
 }
 
