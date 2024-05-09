51e807c5ac8b852f11d0dfeb56c778aadcac544a
GALw395 <galw395@mail.ru>
2024-05-09 17:02:31 +0300

    social_network_les_21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,21 @@
+import s from './Dialogs.module.css'
+
 
 export const Dialogs = () => {
     return (
-        <div>
-            Text dialoga
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Lera</div>
+                <div className={s.dialog}>Nastia</div>
+                <div className={s.dialog}>Lexa</div>
+                <div className={s.dialog}>Marina</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi, Lera</div>
+                <div className={s.message}>Hi, Nastia</div>
+                <div className={s.message}>Hi, Marina</div>
+            </div>
+
         </div>
     )
 }
