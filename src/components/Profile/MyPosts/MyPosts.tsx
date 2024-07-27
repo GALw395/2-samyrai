b0f6d130908ab32ece17dbe051d656f032b8fa7b
GALw395 <galw395@mail.ru>
2024-07-27 20:44:40 +0300

    social_network_les_30-31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react'
 import { profilePageType } from '../../../redux/state'
 import s from './MyPosts.module.css'
 import { Post } from './Post/Post'
@@ -8,7 +9,11 @@ export const MyPosts = (props: profilePageType) => {
 
   let postsElements = props.posts.map(el => <Post message={el.message} likesCount={el.likesCount} />)
 
+  let newPostElement: any = React.createRef()
 
+  let addPost = () => {
+    alert(newPostElement.current.value)
+  } 
 
   return (
 
@@ -16,9 +21,9 @@ export const MyPosts = (props: profilePageType) => {
       <h3>my posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
-        <button>Add new post</button>
+        <button onClick={ addPost }>Add new post</button>
       </div>
       <div>new post</div>
       <div className={s.posts}>
