52f37d8055b17a613aa1832a30d674ec9d2c8fda
GALw395 <galw395@mail.ru>
2024-07-28 19:54:59 +0300

    social_network_les_35

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,4 +1,4 @@
-import { rerenderEntireTree } from "../render"
+let rerenderEntireTree: Function
 
 export type profilePageType = {
     posts: postType[]
@@ -73,6 +73,8 @@ export let addPost = () => {
 export let updateNewPostText = (newText: string) => {
     state.profilePage.newPostText = newText    
     rerenderEntireTree({...state})
+}
 
-
+export const subscribe = (observer: any) => {
+    rerenderEntireTree = observer
 }
